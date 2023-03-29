11e95d26066555fe80c0672acb94ed74d93c2dfd
Nikhil Kumar Singh <rinkuniks@gmail.com>
2023-03-29 21:14:11 +0530

    Complete Profile Changes

--- app/src/main/java/com/nikhil/mynewsample/screen/login/LoginActivity.kt
+++ app/src/main/java/com/nikhil/mynewsample/screen/login/LoginActivity.kt
@@ -21,7 +21,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         binding.dataProvider = viewModel
         binding.btnLogin.text = "Login"
