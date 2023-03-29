65e8129a995e5508a9b05212e2bbb3389c892d11
Nikhil Kumar Singh <rinkuniks@gmail.com>
2023-03-29 16:54:00 +0530

    Register UI and More Modification for Auth UI

---
+++ app/src/main/java/com/nikhil/mynewsample/screen/login/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.nikhil.mynewsample.screen.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nikhil.mynewsample.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
