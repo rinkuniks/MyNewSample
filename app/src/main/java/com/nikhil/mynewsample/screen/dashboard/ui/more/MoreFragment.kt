9161d09924118a36d2cf8db026538d10e13926e7
Nikhil Kumar Singh <rinkuniks@gmail.com>
2023-03-31 15:44:40 +0530

    RecyclerView more UI

--- app/src/main/java/com/nikhil/mynewsample/screen/dashboard/ui/more/MoreFragment.kt
+++ app/src/main/java/com/nikhil/mynewsample/screen/dashboard/ui/more/MoreFragment.kt
@@ -28,10 +28,10 @@ class MoreFragment : Fragment() {
         _binding = FragmentMoreBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMore
-        moreViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textMore
+//        moreViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
