9277a0aec8b46f105de42de751f532f44c3841eb
Nikhil Kumar Singh <rinkuniks@gmail.com>
2023-06-07 18:12:07 +0530

    Animation working

--- app/src/main/java/com/nikhil/mynewsample/CarousalViewActivity.kt
+++ app/src/main/java/com/nikhil/mynewsample/CarousalViewActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.viewpager2.widget.ViewPager2
+import com.airbnb.lottie.LottieAnimationView
 import com.nikhil.mynewsample.adapters.ViewPagerAdapter
+import java.util.ArrayList
 
 class CarousalViewActivity : AppCompatActivity() {
     private lateinit var dot1: ImageView
@@ -14,60 +16,67 @@ class CarousalViewActivity : AppCompatActivity() {
     private lateinit var dot3: ImageView
     private lateinit var viewPager2: ViewPager2
 
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carousal_view)
 
-            viewPager2 = findViewById(R.id.mainViewPager)
-            dot1 = findViewById(R.id.dot1)
-            dot2 = findViewById(R.id.dot2)
-            dot3 = findViewById(R.id.dot3)
+        viewPager2 = findViewById(R.id.mainViewPager)
+        dot1 = findViewById(R.id.dot1)
+        dot2 = findViewById(R.id.dot2)
+        dot3 = findViewById(R.id.dot3)
 
-            val images = listOf(R.color.grey, R.color.black, R.color.purple_500)
-            val myAdapter = ViewPagerAdapter(images)
-            viewPager2.adapter = myAdapter
-            viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageScrolled(
-                    position: Int,
-                    positionOffset: Float,
-                    positionOffsetPixels: Int
-                ) {
-                    changeColor()
-                    super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-                }
+        val myAdapter = ViewPagerAdapter()
+        myAdapter.myAnimeFileName = getAnimeList()
+        viewPager2.adapter = myAdapter
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                changeColor()
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
 
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                }
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
 
-                override fun onPageScrollStateChanged(state: Int) {
-                    changeColor()
-                    super.onPageScrollStateChanged(state)
-                }
+            override fun onPageScrollStateChanged(state: Int) {
+                changeColor()
+                super.onPageScrollStateChanged(state)
+            }
+        })
+    }
 
-            })
-
+    private fun getAnimeList(): ArrayList<String> {
+        return ArrayList<String>().apply {
+            this.add("login_one.json")
+            this.add("new_anime.json")
+            this.add("login_one.json")
         }
+    }
 
+    private fun changeColor() {
+        when (viewPager2.currentItem) {
+            0 -> {
+                dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
+                dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+                dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+            }
 
-        private fun changeColor() {
-            when(viewPager2.currentItem){
-                0->{
-                    dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
-                    dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                    dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                }
-                1->{
-                    dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                    dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
-                    dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                }
-                2->{
-                    dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                    dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
-                    dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
-                }
+            1 -> {
+                dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+                dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
+                dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+            }
+
+            2 -> {
+                dot1.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+                dot2.setBackgroundColor(applicationContext.resources.getColor(R.color.grey))
+                dot3.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
             }
         }
+    }
 }
--- app/src/main/java/com/nikhil/mynewsample/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/nikhil/mynewsample/adapters/ViewPagerAdapter.kt
@@ -5,29 +5,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.nikhil.mynewsample.R
 
-class ViewPagerAdapter(private val images: List<Int>) :
+class ViewPagerAdapter() :
     RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+    var myAnimeFileName = ArrayList<String>()
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewPagerAdapter.ViewPagerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.animation_view,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.animation_view, parent, false)
         return ViewPagerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewPagerAdapter.ViewPagerViewHolder, position: Int) {
-        val myImages = images[position]
-        holder.imageView.setImageResource(myImages)
+        val myImages = myAnimeFileName[position]
+        holder.lottieAnime.setAnimation(myImages)
+//        holder.lottieAnime = myImages
     }
 
     override fun getItemCount(): Int {
-        return images.size
+        return myAnimeFileName.size
     }
 
-    inner class ViewPagerViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
-        val imageView:ImageView = itemView.findViewById(R.id.ivImage)
+    fun updateAnime(fileName: ArrayList<String>){
+        myAnimeFileName = fileName
+        notifyDataSetChanged()
+    }
+
+    inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var lottieAnime: LottieAnimationView = itemView.findViewById(R.id.lav_loginOne)
 
     }
 }
